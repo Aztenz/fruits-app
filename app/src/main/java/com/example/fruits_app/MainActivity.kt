5a8fd67460fd4443b0bf629fc63ff46de6f7809b
Omar Muhammad <aztenz@outlook.com>
2022-09-01 14:02:01 +0200

    [edit] follow up

--- app/src/main/java/com/example/fruits_app/MainActivity.kt
+++ app/src/main/java/com/example/fruits_app/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.fruits_app
 
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myIntent  = Intent(this, )
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            val myIntent  = Intent(this, FruitDetailActivity::class.java)
+            startActivity(myIntent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
     }
 }
