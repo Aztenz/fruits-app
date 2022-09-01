813592a53218a0d4c77270cf37a38719b803444d
Omar Muhammad <aztenz@outlook.com>
2022-09-01 19:19:21 +0200

    [edit] FruitViewHolder
    -changed FruitViewHolder into an inner class in FruitAdaptor.kt file

--- app/src/main/java/com/example/fruits_app/FruitAdapter.kt
+++ app/src/main/java/com/example/fruits_app/FruitAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.fruits_app
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class FruitAdapter(private val fruits: List<Fruit>) : RecyclerView.Adapter<FruitViewHolder>() {
+class FruitAdapter(private val fruits: List<Fruit>) : RecyclerView.Adapter<FruitAdapter.FruitViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FruitViewHolder {
         return FruitViewHolder(LayoutInflater.from(parent.context)
             .inflate(R.layout.item_layout, parent, false))
@@ -18,4 +21,21 @@ class FruitAdapter(private val fruits: List<Fruit>) : RecyclerView.Adapter<Fruit
         return fruits.size
     }
 
+    inner class FruitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val fruitName: TextView
+        private val fruitImage: ImageView
+
+        init {
+            fruitName = itemView.findViewById(R.id.fruitName)
+            fruitImage = itemView.findViewById(R.id.fruitImage)
+            fruitImage.setOnClickListener {
+                fruits[layoutPosition]
+            }
+        }
+
+        fun bindFruit(aFruit: Fruit) {
+            fruitName.text = aFruit.fruitName
+            fruitImage.setImageResource(aFruit.fruitImageModelNumber)
+        }
+    }
 }
