61801c02d2e64c01f28023e19d5fbf2753ac56c0
Omar Muhammad <aztenz@outlook.com>
2022-09-01 19:09:42 +0200

    [edit] follow up
    -added icons to the project
    -finalized the recycleView model

--- app/src/main/java/com/example/fruits_app/MainActivity.kt
+++ app/src/main/java/com/example/fruits_app/MainActivity.kt
@@ -13,11 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView = findViewById(R.id.recycler_view)
-        //val fruitAdaptor = FruitAdapter()
+        val fruitAdaptor = FruitAdapter(fruitsList())
         val button1: Button = findViewById(R.id.button1)
+        recyclerView.adapter = fruitAdaptor
         button1.setOnClickListener {
             val myIntent  = Intent(this, FruitDetailActivity::class.java)
             startActivity(myIntent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
+    }
+    private fun fruitsList(): List<Fruit> {
+        val fruits = mutableListOf<Fruit>()
+        fruits.add(Fruit("Apple", R.drawable.ic_apple))
+        fruits.add(Fruit("Apricot", R.drawable.ic_apricot))
+        fruits.add(Fruit("Avocado", R.drawable.ic_avocado))
+        return fruits
     }
 }
