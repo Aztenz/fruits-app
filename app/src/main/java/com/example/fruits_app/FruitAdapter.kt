f08a48cb1c9761221dd102575163c6401d63703e
Omar Muhammad <aztenz@outlook.com>
2022-09-03 13:47:50 +0200

    Update FruitAdapter.kt

--- app/src/main/java/com/example/fruits_app/FruitAdapter.kt
+++ app/src/main/java/com/example/fruits_app/FruitAdapter.kt
@@ -2,7 +2,6 @@ package com.example.fruits_app
 
 import android.app.Activity
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
