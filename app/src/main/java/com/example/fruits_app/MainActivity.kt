726a9fda9c018fb495d0c864ab47089ba484e3f2
Omar Muhammad <aztenz@outlook.com>
2022-09-04 10:35:12 +0200

    [add] rest of items

--- app/src/main/java/com/example/fruits_app/MainActivity.kt
+++ app/src/main/java/com/example/fruits_app/MainActivity.kt
@@ -19,6 +19,17 @@ class MainActivity : AppCompatActivity() {
         fruits.add(Fruit("Apple", R.drawable.ic_apple, getString(R.string.appleDescription), R.drawable.bg_apple))
         fruits.add(Fruit("Apricot", R.drawable.ic_apricot, "An Apricot", R.drawable.bg_apricots))
         fruits.add(Fruit("Avocado", R.drawable.ic_avocado, "An Avocado", R.drawable.bg_avocado))
+        fruits.add(Fruit("Banana", R.drawable.ic_banana, "An Avocado", R.drawable.bg_banana))
+        fruits.add(Fruit("Blue Berry", R.drawable.ic_blue_berry, "An Avocado", R.drawable.bg_blueberry))
+        fruits.add(Fruit("Cherry", R.drawable.ic_cherry, "An Avocado", R.drawable.bg_cherry))
+        fruits.add(Fruit("Dragon Fruit", R.drawable.ic_dragon_fruit, "An Avocado", R.drawable.bg_dragonfruit))
+        fruits.add(Fruit("Fig", R.drawable.ic_fig, "An Avocado", R.drawable.bg_fig))
+        fruits.add(Fruit("Goji Berry", R.drawable.ic_goji_berry, "An Avocado", R.drawable.bg_gojiberry))
+        fruits.add(Fruit("Grapes", R.drawable.ic_grapes, "An Avocado", R.drawable.bg_grape))
+        fruits.add(Fruit("Kiwi", R.drawable.ic_kiwi, "An Avocado", R.drawable.bg_kiwi))
+        fruits.add(Fruit("Mango", R.drawable.ic_mango, "An Avocado", R.drawable.bg_mango))
+        fruits.add(Fruit("Melon", R.drawable.ic_melon, "An Avocado", R.drawable.bg_melon))
+        fruits.add(Fruit("Watermelon", R.drawable.ic_watermelon, "An Avocado", R.drawable.bg_watermelon))
         return fruits
     }
 }
